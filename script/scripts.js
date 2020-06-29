cd2557c2cae23f1f51a200e9944c569a75f7bba8
lukaszscislowski <lukas.scislowski@gmail.com>
2020-06-29 14:26:23 +0200

    add scripts

--- script/scripts.js
+++ script/scripts.js
@@ -15,13 +15,24 @@ document.addEventListener('DOMContentLoaded', () => {
           // Get the target from the "data-target" attribute
           const target = el.dataset.target;
           const $target = document.getElementById(target);
+          const hero = document.querySelector('.hero');
+          const heroSec = document.querySelector('.hero-secondary');
+          const heroThrd = document.querySelector('.hero-third');
+          const footer = document.querySelector('.footer');
   
           // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
           el.classList.toggle('is-active');
           $target.classList.toggle('is-active');
+          hero.classList.toggle('hero-active');
+          heroSec.classList.toggle('hero-active');
+          heroThrd.classList.toggle('hero-active');
+          footer.classList.toggle('hero-active');
+        
   
         });
       });
     }
   
   });
+
+ 
